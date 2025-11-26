fbd2b3ea71af29d4c08117ca5c82b20f32ddfce9
Utkrisht <utkrisht14@gmail.com>
2025-11-26 21:03:59 +0100

    Logging functions

--- wine_quality_prediction.py
+++ wine_quality_prediction.py
@@ -9,6 +9,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
 import mlflow
 import mlflow.sklearn
+from pathlib import Path
+import os
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
@@ -32,9 +34,13 @@ if __name__ == "__main__":
 
     # Read the wine-quality csv file from local
     data = pd.read_csv(r"C:\Users\utkri\Downloads\Datasets\red-wine-quality.csv")
+    # os.mkdir("data/")
+    data.to_csv("data/red-wine-quality.csv", index=False)
 
     # Split the data into training and test sets. (0.75, 0.25) split.
     train, test = train_test_split(data)
+    train.to_csv("data/train.csv")
+    test.to_csv("data/test.csv")
 
     # The predicted column is "quality" which is a scalar from [3, 9]
     train_x = train.drop(["quality"], axis=1)
@@ -44,24 +50,75 @@ if __name__ == "__main__":
 
     alpha = args.alpha
     l1_ratio = args.l1_ratio
-    exp = mlflow.set_experiment(experiment_name="experiment_1")
 
-    with mlflow.start_run(experiment_id=exp.experiment_id):
-        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
-        lr.fit(train_x, train_y)
+    mlflow.set_tracking_uri(uri="./mytracks")
+    print(f"The set tracking uri is: {mlflow.set_tracking_uri("")}")
 
-        predicted_qualities = lr.predict(test_x)
+    exp = mlflow.set_experiment(
+                                    experiment_name="experiment_3"
+                                      )
+    # get_exp = mlflow.get_experiment(exp_id)
 
-        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+    print("Name: {}".format(exp.name))
+    print("Experiment_id: {}".format(exp.experiment_id))
+    print("Artifact Location: {}".format(exp.artifact_location))
+    print("Tags: {}".format(exp.tags))
+    print("Lifecycle_stage: {}".format(exp.lifecycle_stage))
+    print("Creation timestamp: {}".format(exp.creation_time))
 
-        print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
-        print("  RMSE: %s" % rmse)
-        print("  MAE: %s" % mae)
-        print("  R2: %s" % r2)
 
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_param("l1_ratio", l1_ratio)
-        mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("r2", r2)
-        mlflow.log_metric("mae", mae)
-        mlflow.sklearn.log_model(lr, "mymodel")
+
+    mlflow.start_run()
+
+    tags = {
+        "engineering": "ML Platform",
+        "release.candidate": "RC1",
+        "release.version": "2.0"
+    }
+
+    mlflow.set_tags(tags)
+    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+    lr.fit(train_x, train_y)
+
+    predicted_qualities = lr.predict(test_x)
+
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+    print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+
+    # log parameters
+    params = {
+        "alpha": alpha,
+        "l1_ratio": l1_ratio,
+    }
+
+    mlflow.log_params(params)
+    # log metrics
+    metrics = {
+        "rmse": rmse,
+        "r2": r2,
+        "mae": mae
+    }
+
+    mlflow.log_metrics(metrics)
+
+
+    mlflow.log_param("alpha", alpha)
+    mlflow.log_param("l1_ratio", l1_ratio)
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("r2", r2)
+    mlflow.log_metric("mae", mae)
+    mlflow.sklearn.log_model(lr, "my_new_model")
+
+    # Log artifact
+    mlflow.log_artifacts("data/")
+
+    artifacts_uri = mlflow.get_artifact_uri() # It will return the default artifact directory.
+    print(f"The artifact path is: {artifacts_uri}")
+    mlflow.end_run()
+    run = mlflow.last_active_run()
+    print(f"Active run id is: {run.info.run_id}")
+    print(f"Active run name is: {run.info.run_name}")
